f3dc8d2918db1bb7968757ab9b1efc034f420b50
dvd8ddd <dacodmhuda0125@gmail.com>
2026-09-08 19:02:51 -0600

    Update fuego.py

--- SimulacionBasica/fuego.py
+++ SimulacionBasica/fuego.py
@@ -89,19 +89,19 @@ def efectos_secundarios(model):
         col,fila=bombero.pos
         if model.fuego[fila][col]==2:
             filaSalida, colSalida=model.salidas[0] #guarda primera salida
-        pocaDistancia=abs(fila-filaSalida)+abs(col-colSalida) 
-        for salida in model.salidas: #compara las diferentes salidas
-            filaOpcional, colOpcional=salida
-            distancia=abs(fila-filaOpcional)+abs(col-colOpcional) 
+            pocaDistancia=abs(fila-filaSalida)+abs(col-colSalida) 
+            for salida in model.salidas: #compara las diferentes salidas
+                filaOpcional, colOpcional=salida
+                distancia=abs(fila-filaOpcional)+abs(col-colOpcional) 
             
-            if distancia<pocaDistancia:
-                pocaDistancia=distancia
-                filaSalida=filaOpcional
-                colSalida=colOpcional
-        if bombero.cargando: #aqui es perder la victima mienrtras el bombero la carga
-            model.perdidas+=1
-            bombero.cargando=False
-        model.grid.move_agent(bombero, (colSalida,filaSalida))
+                if distancia<pocaDistancia:
+                    pocaDistancia=distancia
+                    filaSalida=filaOpcional
+                    colSalida=colOpcional
+            if bombero.cargando: #aqui es perder la victima mienrtras el bombero la carga
+                model.perdidas+=1
+                bombero.cargando=False
+            model.grid.move_agent(bombero, (colSalida,filaSalida))
 
     for fila in range(6): #eliminar poi quemadso
         for col in range(8):
